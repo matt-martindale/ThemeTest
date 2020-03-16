3c703e7753c6142fa2f3ff171760d92e21a1ade5
Matt Martindale <martindalematt18@yahoo.com>
2020-03-15 20:42:43 -0600

    initial commit

---
+++ Starts/Starts/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starts
+//
+//  Created by Matthew Martindale on 2/13/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
